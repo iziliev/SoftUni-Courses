7b069410c2f42ce6e764ca48805d0773c5fefa28
Ivaylo Iliev <i_z_iliev@yahoo.com>
2022-02-03 21:43:44 +0200

    Add files via upload

---
+++ C#-Advanced-01.2022/Lab/05-Functional-Programming/02-Sum-Numbers/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _02_Sum_Numbers
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(input.Count());
+            Console.WriteLine(input.Sum());
+        }
+    }
+}
---
+++ C#-Advanced-01.2022/Lab/06-Defining-Classes/01-Car-Manufacturer/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var car = new Car();
+
+            car.Make = "Honda";
+            car.Model = "Civic";
+            car.Year = 1994;
+
+            Console.WriteLine($"{car.Make}");
+            Console.WriteLine($"{car.Model}");
+            Console.WriteLine($"{car.Year}");
+        }
+    }
+}
