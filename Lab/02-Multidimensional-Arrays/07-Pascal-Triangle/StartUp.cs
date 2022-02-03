3fb4a9d7d3c0ccd97b954459c4dac8fc9ce8cb4a
Ivaylo Iliev <i_z_iliev@yahoo.com>
2022-02-03 21:33:42 +0200

    Add files via upload

---
+++ Lab/02-Multidimensional-Arrays/07-Pascal-Triangle/StartUp.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace _07_Pascal_Triangle
+{
+    internal class StartUp
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var matrix = new long[n][];
+
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                matrix[i] = new long[i+1];
+
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    if (i == 0)
+                    {
+                        matrix[i][j] = 1;
+                    }
+                    else if (i == 1)
+                    {
+                        matrix[i][j] = 1;
+                    }
+                    else
+                    {
+                        if (IsInMatrix(matrix, i - 1, j - 1))
+                        {
+                            if (IsInMatrix(matrix,i-1,j))
+                            {
+                                matrix[i][j] = matrix[i - 1][j - 1] + matrix[i - 1][j];
+                            }
+                            else
+                            {
+                                matrix[i][j] = matrix[i - 1][j - 1];
+                            }
+                            
+                        }
+                        else
+                        {
+                            matrix[i][j] = matrix[i - 1][j];
+                        }
+                    }
+                }
+            }
+
+            PrintMatrix(matrix);
+
+        }
+
+        private static void PrintMatrix(long[][] matrix)
+        {
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                Console.WriteLine(String.Join(" ", matrix[i]));
+            }
+        }
+
+        private static bool IsInMatrix(long[][] matrix, int row, int col)
+        {
+            return row >= 0 && row < matrix.Length && col >= 0 && col < matrix[row].Length;
+        }
+    }
+}
---
+++ Lab/04-Streams-Files-and-Directories/05-Extract-Special-Bytes/ExtractBytes.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ExtractBytes
+{
+    public class ExtractBytes
+    {
+        public static void Main()
+        {
+            string binaryFilePath = @"..\..\..\example.png";
+            string bytesFilePath = @"..\..\..\bytes.txt";
+            string outputPath = @"..\..\..\output.bin";
+
+            ExtractBytesFromBinaryFile(binaryFilePath, bytesFilePath, outputPath);
+        }
+
+        public static void ExtractBytesFromBinaryFile(string binaryFilePath, string bytesFilePath, string outputPath)
+        {
+            using FileStream fs = new FileStream(binaryFilePath, FileMode.Open);
+            using StreamReader sr = new StreamReader(bytesFilePath);
+            using FileStream fw = new FileStream(outputPath, FileMode.OpenOrCreate);
+
+            var list = new List<byte>();
+
+            while (!sr.EndOfStream)
+            {
+                list.Add(byte.Parse(sr.ReadLine()));
+            }
+
+            var buffer = new byte[1024];
+
+            while (true)
+            {
+                var currentByte = fs.Read(buffer, 0, buffer.Length);
+
+                if (currentByte == 0)
+                {
+                    break;
+                }
+
+                for (int i = 0; i < buffer.Length; i++)
+                {
+                    if (list.Contains(buffer[i]))
+                    {
+                        for (int j = 0; j < list.Count; j++)
+                        {
+                            if (buffer[i] == list[j])
+                            {
+                                fw.Write(buffer, i, 1);
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+
+    }
+}
