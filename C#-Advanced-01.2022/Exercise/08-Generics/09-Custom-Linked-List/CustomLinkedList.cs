65e38268ebb582106fe7f10ce7756c970551debd
iziliev <i_z_iliev@yahoo.com>
2022-02-05 15:21:00 +0200

    update

---
+++ C#-Advanced-01.2022/Exercise/08-Generics/09-Custom-Linked-List/CustomLinkedList.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomLinkedList
+{
+    public class CustomLinkedList<T>
+    {
+        private const int DefaultLenght = 2;
+
+        private T[] items;
+
+        public CustomLinkedList()
+        {
+            this.items = new T[DefaultLenght];
+        }
+        public int Count { get; private set; }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index>=this.Count)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                return this.items[index];
+            }
+            set
+            {
+                if (index >= this.Count)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                this.items[index]=value;
+            }
+        }
+        public void Add(T item)
+        {
+            if (this.Count == this.items.Length)
+            {
+                Resize();
+            }
+
+            this.items[this.Count] = item;
+            this.Count++;
+        }
+        public T RemoveAt(int index)
+        {
+            if (IsInRange(index))
+            {
+                var element = this.items[index];
+
+                Shrink(index);
+
+                return element;
+            }
+            else
+            {
+                throw new IndexOutOfRangeException();
+            }
+        }
+        public T[] ToArray()
+        {
+            var array = new T[this.Count];
+            for (int i = 0; i < this.Count; i++)
+            {
+                array[i] = this.items[i];
+            }
+            return array;
+        }
+        public void Insert(int index, T item)
+        {
+            if (this.Count == this.items.Length && IsInRange(index))
+            {
+                Resize();
+            }
+            this.Count++;
+
+            for (int i = this.Count; i >= index; i--)
+            {
+                this.items[i] = this.items[i-1];
+            }
+            this.items[index]=item;
+        }
+        public bool Contains(T element)
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                if (element.Equals(this.items[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public void Swap(int firstIndex, int secondIndex)
+        {
+            if (IsInRange(firstIndex) && IsInRange(secondIndex))
+            {
+                var temp = this.items[firstIndex];
+                this.items[firstIndex] = this.items[secondIndex];
+                this.items[secondIndex] = temp;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+        private void Resize()
+        {
+            var array = new T[this.items.Length * 2];
+
+            for (int i = 0; i < this.items.Length; i++)
+            {
+                array[i] = this.items[i];
+            }
+            this.items = array;
+        }
+        private void Shrink(int index)
+        {
+            for (int i = index; i < this.Count-1; i++)
+            {
+                this.items[i] = this.items[i + 1];
+            }
+            this.Count--;
+
+            if (this.Count < this.items.Length / 4)
+            {
+                var array = new T[this.items.Length / 2];
+
+                for (int i = 0; i < this.items.Length; i++)
+                {
+                    array[i] = this.items[i];
+                }
+                this.items = array;
+            }
+        }
+        private bool IsInRange(int index)
+        {
+            return index >= 0 && index < this.Count;
+        }
+    }
+}
---
+++ C#-Advanced-01.2022/Exercise/08-Generics/09-Custom-Linked-List/StartUp.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomLinkedList
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var list = new CustomLinkedList<int>();
+
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            //1-2-3-4
+            Console.WriteLine(String.Join("-",list.ToArray()));
+            //3
+            Console.WriteLine(list.RemoveAt(2));
+            //1-2-4
+            Console.WriteLine(String.Join("-", list.ToArray()));
+            //3
+            Console.WriteLine(list.Count);
+            //true
+            Console.WriteLine(list.Contains(2));
+            //false
+            Console.WriteLine(list.Contains(5));
+            list.Add(5);
+            list.Add(6);
+            //1-2-4-5-6
+            Console.WriteLine(String.Join("-", list.ToArray()));
+            list.Insert(2, 22);
+            //1-2-22-4-5-6
+            Console.WriteLine(String.Join("-", list.ToArray()));
+            list.Swap(2, 5);
+            //1-2-6-4-5-22
+            Console.WriteLine(String.Join("-",list.ToArray()));
+
+        }
+    }
+}
