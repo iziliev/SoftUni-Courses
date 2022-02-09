549507f526428908d376d1a7ef542c1783651979
iziliev <i_z_iliev@yahoo.com>
2022-02-09 17:59:36 +0200

    new exersice

---
+++ C#-Advanced-01.2022/Exercise/09-Iterators-and-Comparators/06-Equality-Logic/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EqualityLogic
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var sortSet = new SortedSet<Person>();
+            var hashSet = new HashSet<Person>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var args = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                var person = new Person(args[0], int.Parse(args[1]));
+
+                sortSet.Add(person);
+                hashSet.Add(person);
+            }
+
+            Console.WriteLine(sortSet.Count);
+            Console.WriteLine(hashSet.Count);
+        }
+    }
+}
