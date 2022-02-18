51e034e60fe1b2be701a361615d6a640cf5a56be
iziliev <i_z_iliev@yahoo.com>
2022-02-18 23:26:15 +0200

    Algorithms Introduction

---
+++ C#/C#-Advanced-01.2022/Lab/12-Algorithms-Introduction/06-Qucksort/StartUp.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Qucksort
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var list = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            var result = QuckSort(list);
+
+            Console.WriteLine(String.Join(" ",result));
+        }
+
+        private static List<int> QuckSort(List<int> list)
+        {
+            if (list.Count<=1)
+            {
+                return list;
+            }
+            int pivot = list[0];
+            var leftList = new List<int>();
+            var rightList = new List<int>();
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i]<pivot)
+                {
+                    leftList.Add(list[i]);
+                }
+                if (list[i]>pivot)
+                {
+                    rightList.Add(list[i]);
+                }
+            }
+            
+            return QuckSort(leftList).Concat(new List<int> { pivot }).Concat(QuckSort(rightList)).ToList();
+        }
+    }
+}
