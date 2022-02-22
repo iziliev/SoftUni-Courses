c7e1bcc619f77584568bf46dfca32155c9e912a7
iziliev <i_z_iliev@yahoo.com>
2022-02-22 16:56:32 +0200

    Exam 20.02.2022

---
+++ C#/C#-Advanced-01.2022/Exam-20-02-2022/01-Bakery-Shop/StartUp.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BakeryShop
+{
+    public  class StartUp
+    {
+        public static Dictionary<string, double> bakeryProducts = new Dictionary<string, double>()
+        {
+            {"Croissant",50 },
+            {"Muffin",40 },
+            {"Baguette",30 },
+            {"Bagel",20 },
+        };
+
+        public static void Main()
+        {
+            var waters = new Queue<double>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray());
+
+            var flours = new Stack<double>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray());
+
+            var bakedProducts = new Dictionary<string, int>()
+            {
+                {"Croissant",0 },
+                {"Muffin",0 },
+                {"Baguette",0 },
+                {"Bagel",0 }
+            };
+
+            while (waters.Count>0&&flours.Count>0)
+            {
+                var currentWater = waters.Dequeue();
+                var currentFlour = flours.Pop();
+
+                var waterPercent = (currentWater * 100) / (currentWater + currentFlour);
+                var flourPercent = 100 - waterPercent;
+
+                if (!bakeryProducts.ContainsValue(waterPercent))
+                {
+                    var differentFlour = currentFlour - currentWater;
+                    flours.Push(differentFlour);
+                    bakedProducts["Croissant"]++;
+                }
+                else
+                {
+                    var key = bakeryProducts.Where(x => x.Value == waterPercent).FirstOrDefault().Key;
+                    bakedProducts[key]++;
+                }
+            }
+
+            foreach (var product in bakedProducts.Where(x=>x.Value>0).OrderByDescending(x=>x.Value).ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"{product.Key}: {product.Value}");
+            }
+            if (waters.Count==0)
+            {
+                Console.WriteLine("Water left: None");
+            }
+            else
+            {
+                Console.WriteLine($"Water left: {string.Join(", ",waters)}");
+            }
+            if (flours.Count == 0)
+            {
+                Console.WriteLine("Flour left: None");
+            }
+            else
+            {
+                Console.WriteLine($"Flour left: {string.Join(", ", flours)}");
+            }
+        }
+    }
+}
---
+++ C#/C#-Advanced-01.2022/Exam-20-02-2022/03-Fishing-Net/Net.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FishingNet
+{
+    public class Net
+    {
+        private List<Fish> fishes;
+        public Net(string material, int capacity)
+        {
+            this.Material = material;
+            this.Capacity = capacity;
+            this.fishes = new List<Fish>();
+        }
+        public IReadOnlyCollection<Fish> Fish => this.fishes;
+        public string Material { get; set; }
+        public int Capacity { get; set; }
+        public int Count => this.fishes.Count;
+        public string AddFish(Fish fish)
+        {
+            if (this.Count < this.Capacity)
+            {
+                if (!string.IsNullOrWhiteSpace(fish.FishType) && fish.Length>0&&fish.Weight>0)
+                {
+                    this.fishes.Add(fish);
+                    return $"Successfully added {fish.FishType} to the fishing net.";
+                }
+
+                return "Invalid fish.";
+            }
+            return "Fishing net is full.";
+        }
+        public bool ReleaseFish(double weight)
+        {
+            var currentFish = this.fishes.FirstOrDefault(x => x.Weight == weight);
+            if (currentFish!=null)
+            {
+                this.fishes.Remove(currentFish);
+                return true;
+            }
+            return false;
+        }
+        public Fish GetFish(string fishType)
+        {
+            return this.fishes.FirstOrDefault(x => x.FishType == fishType);
+        }
+        public Fish GetBiggestFish()
+        {
+            return this.fishes.OrderByDescending(x=>x.Length).FirstOrDefault();
+        }
+        public string Report()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"Into the {this.Material}:");
+            foreach (var fish in this.fishes.OrderByDescending(x=>x.Length))
+            {
+                sb.AppendLine(fish.ToString());
+            }
+            return sb.ToString().Trim();
+        }
+    }
+}
