7c2501d6fa68a634ef873da41b5be94ad6460599
iziliev <i_z_iliev@yahoo.com>
2022-03-10 21:39:31 +0200

    upload 05

---
+++ C#/C#-OOP-02.2022/Lab/05-Exception-Handling/03-Cards/ICard.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03_Cards
+{
+    public interface ICard
+    {
+        public string Face { get; }
+        public string Suit { get; }
+    }
+}
